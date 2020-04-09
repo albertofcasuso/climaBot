dfc6dd19d43169905ed815d1ff4e4d28fd2f6e76
Casuso <hi@casuso.dev>
2020-04-09 12:34:49 -0500

    ejemplo de config

---
+++ config_example.js
@@ -0,0 +1,12 @@
+exports.twitConfig = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
+    strictSSL: true,
+}
+
+exports.apiConfig = {
+    APIKey:'', //key api
+    cityID:'3530597' //codigo ciudad
+}
