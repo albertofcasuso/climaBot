5fd897ff7c50687a05593b00bd22f4337dee3a1b
Casuso <hi@casuso.dev>
2020-06-13 10:20:59 -0500

    nueva config example

--- config_example.js
+++ config_example.js
@@ -1,12 +1,17 @@
 exports.twitConfig = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: '',
-    strictSSL: true,
-}
+  consumer_key: "",
+  consumer_secret: "",
+  access_token: "",
+  access_token_secret: "",
+  strictSSL: true,
+};
 
 exports.apiConfig = {
-    APIKey:'', //key api
-    cityID:'3530597' //codigo ciudad
-}
+  APIKey: "", //key api
+};
+
+//coordenadas de la ciudad
+exports.coords = {
+  lat: "19.4284706",
+  lon: "-99.1276627",
+};
